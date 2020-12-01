1a81b4b4fe1d486e40e64db0110032bc4c1a1ab7
KoalaV2 <theo.johanson@gmail.com>
2020-12-01 15:49:56 +0100

    yes

--- 01/part1.py
+++ 01/part1.py
@@ -6,8 +6,8 @@ integers = []
 for str in file.readlines():
     integers.append(int(str))
     print(integers)
-    for numbers in integers:
-        for numbers2 in integers:
-            if numbers + numbers2 == 2020:
-                print("Answer: ",numbers * numbers2)
-                exit()
+for numbers in integers:
+    for numbers2 in integers:
+        if numbers + numbers2 == 2020:
+            print("Answer: ",numbers * numbers2)
+            exit()
