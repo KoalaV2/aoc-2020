6019435ee479acb373874202eddc0ec159d0f9bd
KoalaV2 <theo.johanson@gmail.com>
2020-12-01 15:32:33 +0100

    unindexed so it doest parse all the numbers everytime

--- 01/part2.py
+++ 01/part2.py
@@ -5,10 +5,10 @@ file = open('numbers.txt', 'r')
 integers = []
 for str in file.readlines():
     integers.append(int(str))
-    print(integers)
-    for numbers in integers:
-        for numbers2 in integers:
-            for numbers3 in integers:
-                if numbers + numbers2 + numbers3 == 2020:
-                    print("Answer: ",numbers * numbers2 * numbers3 )
-                    exit()
+#    print(integers)
+for numbers in integers:
+    for numbers2 in integers:
+        for numbers3 in integers:
+            if numbers + numbers2 + numbers3 == 2020:
+                print("Answer: ",numbers * numbers2 * numbers3 )
+                exit()
